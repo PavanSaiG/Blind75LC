839bf6de474f2c6fd4c0502a483d66e1a342e6ea
PavanSaiG <86244738+PavanSaiG@users.noreply.github.com>
2024-08-22 16:32:02 +0530

    1732

---
+++ 1732.find-the-highest-altitude.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1732 lang=java
+ *
+ * [1732] Find the Highest Altitude
+ */
+
+// @lc code=start
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n = gain.length;
+        int[] pf = new int[n];
+        pf[0] = gain[0];
+        for(int i=1;i<n;i++)
+        {
+            pf[i] = pf[i-1] + gain[i];
+        }
+        int max = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(pf[i]>max)
+            max = pf[i];
+        }
+        return max;
+    }
+}
+// @lc code=end
+
