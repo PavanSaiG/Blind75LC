9f01b54dc5a4988a88f7986c9b865cde858d19ff
PavanSaiG <86244738+PavanSaiG@users.noreply.github.com>
2024-08-29 08:16:06 +0530

    605

--- 605.can-place-flowers.java
+++ 605.can-place-flowers.java
@@ -7,9 +7,16 @@
 // @lc code=start
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        boolean ans = false;
-        
-        return ans;
+        for (int i = 0; i < flowerbed.length; i++) {
+            boolean left = i == 0 || flowerbed[i - 1] == 0;
+            boolean right = i == flowerbed.length - 1 || flowerbed[i + 1] == 0;
+            
+            if (left && right && flowerbed[i] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;        
     }
 }
 // @lc code=end
